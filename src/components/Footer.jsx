8a00c2cde9c17f3c3b67c9f23ce98ca82c172703
Shamin Ahsan <shamin.ahsan.01@gmail.com>
2026-08-07 03:56:55 -0400

    Date:07/08/2026 Dev:Anaf Commit:Added createEventBtn and styles.(btn not yet functional)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,46 +5,71 @@ import { NavLink } from "react-router";
  * $$$-Most Recent Change: 07/08/2026, [Md Shamin Ahsan Anaph]
  * $$$-Method Description:
  *    Renders the bottom navigation bar. Shows different links
- * $$$-Functions Using This Method: 
- *    none — this is the top-level exported component, 
+ * $$$-Functions Using This Method:
+ *    none — this is the top-level exported component,
  * rendered by Layout
- * $$$-Description of Variables: 
+ * $$$-Description of Variables:
  *    Repeating class styles grouped into class variables
  *
  * */
-export default function Footer({ user, onLogout }) {
-  const linkClass = ({ isActive }) =>
-    `px-3 py-2 rounded-md text-sm font-medium ${
-      isActive ? "text-(--accent)" : "hover:text-(--text-h)"
-    }`;
+export default function Footer() {
 
-  const footerIContClass = "flex flex-col items-center";
+
+  //Repeating styles grouped together
+  const footerIContClass = "flex flex-col items-center mr-3 ml-3";
   const footerIconClass = "w-10 bg-white rounded-full p-2";
+  const footerCreatebtn = "flex items-center w-10 h-10 bg-white rounded-none rotate-45 p-2";
   const footerTextClass = "font-bold text-xs";
+
+
+  /**
+ * $$$-Funtion Creation: 07/08/2026, [Md Shamin Ahsan Anaph]
+ * $$$-Most Recent Change: 07/08/2026, [Md Shamin Ahsan Anaph]
+ * $$$-Method Description:
+ *    Gets IsActive from the NavLink component and renders different 
+ * style based on the current active tab.
+ * $$$-Component Using This Function:
+ *    Footer-Navlink components
+ * $$$-Description of Variables:
+ *    Repeating class styles grouped into class variables
+ *
+ * */
+  const linkClass = ({ isActive }) =>
+    `${footerIconClass} ${isActive ? "bg-blue-300" : "bg-white"}`;
 
   return (
     <footer className="border-b border-(--border) bg-black border-2 border-b-blue-50">
-      <nav className="mx-auto flex flex-row max-w-3xl items-center justify-evenly gap-2 px-4 py-3">
+      <nav className="mx-auto flex flex-row max-w-3xl items-center justify-center-safe gap-2 px-4 py-3">
+        
+        {/* Discover route, navigates to the discover events page */}
         <span className={footerIContClass}>
-          <NavLink className="w-10 bg-white rounded-full p-2">
+          <NavLink className={linkClass} to='/protected'>
             <img src="src\assets\interface_icons\search.svg" alt="" />
           </NavLink>
           <p className="font-bold text-xs">Discover</p>
         </span>
+
         <span className={footerIContClass}>
-          <NavLink className={footerIconClass}>
+          <NavLink className={linkClass}>
             <img src="src\assets\interface_icons\mapIcon.svg" alt="" />
           </NavLink>
           <p className={footerTextClass}>Map</p>
         </span>
+
+        {/* Create event route, navigates to the create event page*/}
+          <NavLink className={`${footerCreatebtn} -translate-y-10`}>
+            <img className="rotate-45" src="src\assets\interface_icons\createIcon.png" alt="" />
+          </NavLink>
+
+
         <span className={footerIContClass}>
-          <NavLink className={footerIconClass}>
+          <NavLink className={linkClass}>
             <img src="src\assets\interface_icons\messageIcon.svg" alt="" />
           </NavLink>
           <p className={footerTextClass}>Chat</p>
         </span>
         <span className={footerIContClass}>
-          <NavLink className={footerIconClass}>
+          <NavLink className={linkClass}>
             <img src="src\assets\interface_icons\user.svg" alt="" />
           </NavLink>
           <p className={footerTextClass}>Profile</p>
