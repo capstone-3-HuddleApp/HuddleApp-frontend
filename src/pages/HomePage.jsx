116bad2a2b8f5ca031e464254c1e298b55ab7091
Shamin Ahsan <shamin.ahsan.01@gmail.com>
2026-08-17 21:52:53 -0400

    trigger redeploy

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router';
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col gap-4 max-w-sm rounded-2xl border border-[#f4c96b] bg-[#f8d8aa] p-8 mt-10 shadow-sm">
+    <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center gap-4 w-[80vw] md:w-[50vw] lg:w-[40vw] h-[60vh] rounded-2xl border border-[#f4c96b] bg-[#f8d8aa] shadow-sm">
       <div className="flex flex-col gap-2">
-        <h1 className="text-2xl font-bold text-[#f2a451]">Huddle</h1>
+        <h1 className="text-4xl font-bold text-[#f2a451]">Huddle</h1>
         <h2 className="text-sm font-medium text-[#7d8794]">
           Find your people, no fee required
         </h2>
-        <p className="text-sm leading-relaxed text-[#29272b]">
+        <p className="!p-10 text-sm leading-relaxed text-[#29272b]">
           A free, community-run alternative to paid event platforms — helping
           college students and working adults discover and organize pickup
           sports, hobby meetups, and hangouts at public venues, with decisions
